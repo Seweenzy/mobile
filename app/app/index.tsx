232e10819547b4dd7eeb4e99fea45a9d7e7e41af
Seweenzy <koluwaseunemmanuel@gmail.com>
2026-01-09 16:03:17 -0800

    Update index.tsx

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -10,9 +10,9 @@ export default function Index() {
         backgroundColor: "#f7a8a8",
       }}
     >
-      <Text style={{ fontSize: 20, fontWeight: "bold", padding:30 }}>Hello, my name is Seweenzy and I welcome you to my first mobile app!</Text>
+      <Text style={{ fontSize: 20, fontWeight: "bold", padding:30, color:'#ffffff'}}>Hello, my name is Seweenzy and I welcome you to my first mobile app!</Text>
       
-      <Text style={{ fontSize: 16, fontWeight: "bold", marginTop:5}}>Thanks for stopping by!</Text>
+      <Text style={{ fontSize: 16, fontWeight: "bold", marginTop:5, color:'#ffffff'}}>Thanks for stopping by!</Text>
     </View>
   );
 }
