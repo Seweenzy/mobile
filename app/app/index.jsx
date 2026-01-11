0494834a12d67310016cece3e3edf10a947009e7
Seweenzy <koluwaseunemmanuel@gmail.com>
2026-01-11 15:09:06 -0800

    updated

--- app/app/index.jsx
+++ app/app/index.jsx
@@ -1,4 +1,4 @@
-import { Text, View, Image } from "react-native";
+import { Text, Image } from "react-native";
 
 import {SafeAreaView} from 'react-native-safe-area-context';
 
@@ -10,15 +10,10 @@ export default function Index() {
         backgroundColor: "#f7a8a8",
       }}
     >
-      <Text 
-      style={{ fontSize: 20, fontWeight: "bold", paddingHorizontal:30, color:'#ffffff'}}>
-        Hello, my name is Seweenzy and I welcome you to my first mobile app!</Text>
-      
-      <Text 
-      style={{ fontSize: 16, fontWeight: "bold", marginTop:5, color:'#ffffff', paddingHorizontal:30}}>
-        Thanks for stopping by!</Text>
+      <Text>'English (US)'</Text>
 
-      <Image style={{ width: 100, height: 100, marginTop: 20, marginHorizontal:30}} source={require('../assets/images/icon.png')}/>
+
+
     </SafeAreaView>
   );
 }
