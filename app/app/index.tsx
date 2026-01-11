d4909179422c353a947c49f3fe4a9c49136f5e09
Seweenzy <koluwaseunemmanuel@gmail.com>
2026-01-11 14:15:30 -0800

    Update index.tsx

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,18 +1,18 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
         backgroundColor: "#f7a8a8",
       }}
     >
       <Text style={{ fontSize: 20, fontWeight: "bold", padding:30, color:'#ffffff'}}>Hello, my name is Seweenzy and I welcome you to my first mobile app!</Text>
       
       <Text style={{ fontSize: 16, fontWeight: "bold", marginTop:5, color:'#ffffff'}}>Thanks for stopping by!</Text>
+
+      <Image source={require('../assets/images/icon.png')}/>
     </View>
   );
 }
