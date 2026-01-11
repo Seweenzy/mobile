3a5c51f643f24aa2c67661c9dd637c0d75ed120c
Seweenzy <koluwaseunemmanuel@gmail.com>
2026-01-11 15:34:30 -0800

    Update index.jsx

--- app/app/index.jsx
+++ app/app/index.jsx
@@ -1,4 +1,4 @@
-import { Text, Image } from "react-native";
+import { Text, TextInput, TouchableOpacity } from "react-native";
 
 import {SafeAreaView} from 'react-native-safe-area-context';
 
@@ -7,10 +7,35 @@ export default function Index() {
     <SafeAreaView
       style={{
         flex: 1,
-        backgroundColor: "#f7a8a8",
+        backgroundColor: "white",
+        paddingHorizontal: 20,
+        paddingTop: 10,
       }}
     >
-      <Text>'English (US)'</Text>
+      <Text>English (US)</Text>
+
+      <TextInput style={{ height: 60, borderColor: 'grey', borderWidth: 1 , borderRadius: 8, margin: 10, paddingLeft: 10}} 
+      placeholder="Mobile number or email"/>
+
+        <TextInput style={{ height: 60, borderColor: 'grey', borderWidth: 1 , borderRadius: 10, margin: 10, paddingLeft: 10, marginTop: 10}} 
+      placeholder="Password"/>
+
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#1877F2",
+          height: 50,
+          borderRadius: 30,
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: 8,
+        }}
+      >
+        <Text style={{ color: "white", fontSize: 18, fontWeight: "bold" }}>
+          Log In
+        </Text>
+      </TouchableOpacity>
+
+      <Text>Forgot Password?</Text>
 
 
 
